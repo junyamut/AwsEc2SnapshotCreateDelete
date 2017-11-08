d5a2a6f904700381859e34fb3eab08af4bfb0b68
Jose Yamut <junyamut@users.noreply.github.comgit>
2017-11-08 17:59:03 +0800

    Added interface for  task classes

---
+++ app/Interfaces/TaskTemplate.php
@@ -0,0 +1,14 @@
+<?php
+namespace Ec2SnapshotsManagement\Interfaces;
+
+interface TaskTemplate
+{
+    public static function getName();
+    public static function getDescription();
+    public static function getLogMessages();
+    public function execute();
+    public function setAppSettings($appSettings);
+    public function setAwsCredentials($awsCredentials);
+}
+
+?>
